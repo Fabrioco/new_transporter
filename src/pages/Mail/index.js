9620e14cc121e5445c4e1b098d3aa782773d98fc
Fabricio Lopes <100289975+Fabrioco@users.noreply.github.com>
2024-02-13 23:14:07 +0000

    itens no carrinho

--- src/pages/Mail/index.js
+++ src/pages/Mail/index.js
@@ -1,55 +1,104 @@
-import { toast } from "react-toastify"
-import "./mail.css"
+import { toast } from "react-toastify";
+import "./mail.css";
 
 export default function Mail() {
+  function handleAdd(nomeProduto) {
+    toast.success("Item adicionado com sucesso")
+    // Obtém o array de produtos do localStorage ou cria um novo se não existir
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-    function handleAdd() {
-        toast.success("Adicionado ao carrinho")
-    }
+    // Adiciona o novo produto ao array
+    carrinho.push(nomeProduto);
 
-    return (
-        <div>
-            <main className="main-mail">
-                <h1 className="title-mail">Loja</h1>
-                <div className="main-products">
-                    <div className="product">
-                        <img src={'https://m.media-amazon.com/images/I/81fNQ8MkOjL.__AC_SX300_SY300_QL70_ML2_.jpg'} alt="paper.jpg" />
-                        <h3>Papel</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                    <div className="product">
-                        <img src={'https://m.media-amazon.com/images/I/51jw9rgSoZL.__AC_SX300_SY300_QL70_ML2_.jpg'} alt="pen.jpg" />
-                        <h3>Caneta</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                    <div className="product">
-                        <img src={'https://m.media-amazon.com/images/I/61xLyYajSXL.__AC_SY300_SX300_QL70_ML2_.jpg'} alt="eraser.jpg" />
-                        <h3>Borracha</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                    <div className="product">
-                        <img src={'https://m.media-amazon.com/images/I/4105Jd8GpwL.__AC_SX300_SY300_QL70_ML2_.jpg'} alt="pencil.jpg" />
-                        <h3>Lápis</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                    <div className="product">
-                        <img src={"https://m.media-amazon.com/images/I/41MAVv4bJPL.__AC_SX300_SY300_QL70_ML2_.jpg"} alt="notebook.jpg" />
-                        <h3>Caderno</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                    <div className="product">
-                        <img src={"https://m.media-amazon.com/images/I/41B9GfM1kEL.__AC_SX300_SY300_QL70_ML2_.jpg"} alt="scissors.jpg" />
-                        <h3>Tesoura</h3>
-                        <p>Este produto está disponivel</p>
-                        <button onClick={handleAdd} >Adicionar ao carrinho</button>
-                    </div>
-                </div>
-            </main>
+    // Salva o array atualizado no localStorage
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
+
+  return (
+    <div>
+      <main className="main-mail">
+        <h1 className="title-mail">Loja</h1>
+        <div className="main-products">
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/81fNQ8MkOjL.__AC_SX300_SY300_QL70_ML2_.jpg"
+              }
+              alt="paper.jpg"
+            />
+            <h3>Papel</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Papel")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/51jw9rgSoZL.__AC_SX300_SY300_QL70_ML2_.jpg"
+              }
+              alt="pen.jpg"
+            />
+            <h3>Caneta</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Caneta")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/61xLyYajSXL.__AC_SY300_SX300_QL70_ML2_.jpg"
+              }
+              alt="eraser.jpg"
+            />
+            <h3>Borracha</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Borracha")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/4105Jd8GpwL.__AC_SX300_SY300_QL70_ML2_.jpg"
+              }
+              alt="pencil.jpg"
+            />
+            <h3>Lápis</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Lápis")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/41MAVv4bJPL.__AC_SX300_SY300_QL70_ML2_.jpg"
+              }
+              alt="notebook.jpg"
+            />
+            <h3>Caderno</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Caderno")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product">
+            <img
+              src={
+                "https://m.media-amazon.com/images/I/41B9GfM1kEL.__AC_SX300_SY300_QL70_ML2_.jpg"
+              }
+              alt="scissors.jpg"
+            />
+            <h3>Tesoura</h3>
+            <p>Este produto está disponivel</p>
+            <button onClick={() => handleAdd("Tesoura")}>
+              Adicionar ao carrinho
+            </button>
+          </div>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
--- src/pages/subpages/Cart/index.js
+++ src/pages/subpages/Cart/index.js
@@ -1,32 +1,70 @@
-import { useNavigate } from 'react-router-dom'
-import './cart.css'
-import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+import "./cart.css";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
+  function backMail() {
+    navigate("/mail");
+  }
 
-    const navigate = useNavigate();
-    function backMail() {
-        navigate('/mail')
+  function finishBuy() {
+    toast.success("Compra realizada com sucesso");
+  }
+
+  useEffect(() => {
+    function loadItems() {
+      const carrinho = localStorage.getItem("carrinho");
+      if (carrinho) {
+        setItems(JSON.parse(carrinho));
+      } else {
+        setItems([]);
+      }
     }
+    loadItems();
+  }, []);
 
-    function finishBuy() {
-        toast.success("Compra realizada com sucesso")
-    }
+  function handleClear(index){
+    const novosItens = [...items]
+    novosItens.splice(index, 1)
+    setItems(novosItens)
+    if (novosItens.length > 0) {
+        localStorage.setItem("carrinho", JSON.stringify(novosItens));
+      } else {
+        localStorage.removeItem("carrinho");
+      }
+}
 
-
-    return (
-        <div className='allCart'>
-            <h1>Carrinho</h1>
-            <div className='content-cart'>
-                <p>Infelizmente você ainda não adicionou nada no carrinho. Volte a loja e adicione
-                </p>
-                <button
-                    className='btnCart' id='finishCart'
-                    style={{ marginBottom: "10px" }}
-                    onClick={finishBuy}>Finalizar compra
-                </button>
-                <button className='btnCart' onClick={backMail}>Voltar</button>
-            </div>
+  return (
+    <div className="allCart">
+      <h1>Carrinho</h1>
+      <div className="content-cart">
+        <div>
+          <ul>
+            {items.length > 0 ? (
+              items.map((item, index) => <li key={index}>{item} <button onClick={() => handleClear(index)}>Excluir</button></li>)
+            ) : (
+              <p>
+                Infelizmente você ainda não adicionou nada no carrinho. Volte a
+                loja e adicione
+              </p>
+            )}
+          </ul>
         </div>
-    )
+        <button
+          className="btnCart"
+          id="finishCart"
+          style={{ marginBottom: "10px" }}
+          onClick={finishBuy}
+        >
+          Finalizar compra
+        </button>
+        <button className="btnCart" onClick={backMail}>
+          Voltar
+        </button>
+      </div>
+    </div>
+  );
 }
